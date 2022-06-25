840c4cec81a1894314d874c23323c8b5911bd261
Virgold <virgoanchuks@gmail.com>
2022-06-25 03:07:22 +0100

    added css media query

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,7 @@ export default function Sidebar(props){
     return(
         <>
         <Header/>
-        <div className="col-md-3 col-lg-2 d-md-block py-4 text-dark bg shadow-sm bg-liht sidebar-collapse collapse vh-100" id="sidebarMenu">
+        <nav className="col-md-3 col-lg-2 d-md-block py-4 text-dark bg shadow-sm bg-liht sidebar collapse vh-10" id="sidebarMenu">
             <a href="/" className="navbar-brand me-0 px-3">
                 <span className="iconify fs-1 icons" data-icon="simple-icons:welcometothejungle"></span>
             </a>
@@ -25,13 +25,13 @@ export default function Sidebar(props){
                     </a>
                 </li>
                 <li className="nav-item" onClick={props.handleClick3}>
-                    <a href='.' className="nav-link text-dark fs-5">
+                    <a href='/#' className="nav-link text-dark fs-5">
                     <span className="iconify icons me-4" data-icon="akar-icons:people-group"></span>
                     All
                     </a>
                 </li>
             </ul>
-        </div>
+        </nav>
         </>
     )
 }
